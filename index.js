5df1c10ebfd9134549c62760ccd842bfa65f0ed9
maxjmar <86731281+maxjmar@users.noreply.github.com>
2022-01-25 16:19:06 -0500

    changed api to images and updated some css and html

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const apodUrl = 'https://api.nasa.gov/planetary/apod?api_key='
 const apodKey = vault.nasaApiKey
-const apodStDate = '&start_date=2022-01-01'
+const apodStDate = '&start_date=2021-12-29&end_date=2022-01-09'
 
 fetch(`${apodUrl}${apodKey}${apodStDate}`)
 .then(response => response.json())
 .then(apodStuff =>{
-    const i= apodStuff.length -1
-    console.log(apodStuff[i])
+    console.log(apodStuff)
 })
